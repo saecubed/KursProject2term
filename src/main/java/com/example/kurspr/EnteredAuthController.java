fb11d184c01c50a245b054cea38e8bae5ee94f0d
BerdnikovaVM <saecubed@gmail.com>
2022-07-08 23:59:07 +0300

    enteredAuth screen without switch

---
+++ src/main/java/com/example/kurspr/EnteredAuthController.java
@@ -0,0 +1,80 @@
+package com.example.kurspr;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import static com.example.kurspr.MainApplication.table_quotes;
+
+public class EnteredAuthController implements Initializable {
+    @FXML
+    private Button backToAuth;
+    @FXML
+    private TableView<Quote> table;
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    @FXML
+    private TableColumn<Quote, String> date;
+
+    @FXML
+    private TableColumn<Quote, Integer> professor_id;
+
+    @FXML
+    private TableColumn<Quote, String> quote;
+
+    @FXML
+    private TableColumn<Quote, Integer> subject_id;
+
+    @FXML
+    private Button quotesManagement;
+
+    @FXML
+    private Button profileManagement;
+
+    ObservableList<Quote> guest_quotes = FXCollections.observableArrayList(table_quotes.quotes);
+
+    @FXML
+    void switchToAuth(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("authScr.fxml"));
+        stage = (Stage)(((Node) event.getSource()).getScene().getWindow());
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void switchToQM(ActionEvent event) {
+
+    }
+
+    @FXML
+    void switchToPM(ActionEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        date.setCellValueFactory(new PropertyValueFactory<Quote, String>("date"));
+        professor_id.setCellValueFactory(new PropertyValueFactory<Quote, Integer>("professor_id"));
+        quote.setCellValueFactory(new PropertyValueFactory<Quote, String>("quote"));
+        subject_id.setCellValueFactory(new PropertyValueFactory<Quote, Integer>("subject_id"));
+        table.setItems(guest_quotes);
+    }
+}
