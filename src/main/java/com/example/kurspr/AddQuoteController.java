a529d6fed3d64da74f8901fd1447e05259f9d602
BerdnikovaVM <saecubed@gmail.com>
2022-07-09 02:28:00 +0300

    quote adding screen empty

---
+++ src/main/java/com/example/kurspr/AddQuoteController.java
@@ -0,0 +1,44 @@
+package com.example.kurspr;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class AddQuoteController {
+
+    @FXML
+    private DatePicker dateField;
+
+    @FXML
+    private Label dateText;
+
+    @FXML
+    private Label message;
+
+    @FXML
+    private TextField professorField;
+
+    @FXML
+    private Label professorText;
+
+    @FXML
+    private Button publishButton;
+
+    @FXML
+    private TextField quoteField;
+
+    @FXML
+    private Label quoteText;
+
+    @FXML
+    private TextField subjectField;
+
+    @FXML
+    private Label subjectText;
+
+    @FXML
+    private Button toQMButton;
+
+}
